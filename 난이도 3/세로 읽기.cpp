d9f3e19ec2d0743dbdbb671aeee45cd84e28b549
sjoon627 <48435096+sjoon627@users.noreply.github.com>
2020-02-06 23:25:49 +0900

    .

--- 난이도 3/세로 읽기.cpp
+++ 난이도 3/세로 읽기.cpp
@@ -1,1 +1,19 @@
-#include <stdio.h> 
+#include <stdio.h>
+int main() {
+	int n,i,j;
+	int arr[20][20]={0};
+
+ 	scanf("%d",&n);
+	getchar();
+
+	for(i=0 ; i<n ; ++i){
+		for(j=0 ; j<n ; ++j){
+			scanf("%c",&arr[i][j]);
+
+			printf("%c\n",arr[i][j]);
+			printf("%d %d\n",i,j);
+		}
+	}
+	
+	return 0;
+}
