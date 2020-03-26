bc6cbe8b0aa555a0d64cd7b0eaa7d4b5d25b3a88
sjoon627 <48435096+sjoon627@users.noreply.github.com>
2020-03-26 23:46:37 +0900

    .

--- 난이도 2/지뢰찾기.cpp
+++ 난이도 2/지뢰찾기.cpp
@@ -1,1 +1,53 @@
-#include
+#include <stdio.h>
+int main() {
+	char arr[100][100];
+	char ans[100][100]={0};
+	int i,j,m,n;
+	
+	scanf("%d %d",&m,&n);
+	
+	for(i=0;i<m;i++){
+			scanf("%s",arr[i]);
+			for(j=0;j<n;j++){
+				ans[i][j]='0';
+			}
+	}
+	
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			if(arr[i][j]=='*'){
+				ans[i][j]='*';
+				
+				if(ans[i][j+1]!='*')
+				ans[i][j+1]++;
+				
+				if(ans[i][j-1]!='*')
+				ans[i][j-1]++;
+				
+				if(ans[i+1][j]!='*')
+				ans[i+1][j]++;
+				
+				if(ans[i-1][j]!='*')
+				ans[i-1][j]++;
+				
+				if(ans[i+1][j+1]!='*')
+				ans[i+1][j+1]++;
+				
+				if(ans[i+1][j-1]!='*')
+				ans[i+1][j-1]++;
+				
+				if(ans[i-1][j+1]!='*')
+				ans[i-1][j+1]++;
+				
+				if(ans[i-1][j-1]!='*')
+				ans[i-1][j-1]++;
+			
+			}
+		}
+	}
+	
+		for(i=0;i<m;i++){
+			printf("%s",ans[i]);
+			printf("\n"); 
+	}
+}
